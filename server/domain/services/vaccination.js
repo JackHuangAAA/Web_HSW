be76dfd6972374da7d01510208e0b47c79340aa3
hsw1048896938 <1048896938@qq.com>
2019-10-08 16:33:12 +0800

    增加接种记录路由，接种记录增改查。 server\domain\services\vaccination.js server\web\routes\vaccination.js

--- server/domain/services/vaccination.js
+++ server/domain/services/vaccination.js
@@ -1,38 +1,94 @@
 /**
- * Created by Administrator on 2019/9/24.
+ * Created by Administrator on 2019/10/07 0041.
  */
-const logger = Libs.logger.getLogger('vaccination');
 const moment = require('moment');
+const logger = Libs.logger.getLogger('vaccination');
 
 module.exports = {
 
     /**
-     * 
-     * 
-     * @param {any} requestBody 
-     * @returns 
+     * 查询接种记录
+     * @param requestBody
+     * @returns
      */
-    queryVaccinationByCustomerCode: async function (requestBody) {
+    queryVaccination: async function(requestBody){
+        logger.debug(`queryVaccination param: ${JSON.stringify(requestBody)}`);
         let query = [];
-        let today = moment();
-        query.push({ "createDate": { '$gte': today.startOf('day').toDate(), '$lte': today.endOf('day').toDate() } });
-        if (!_.isEmpty(requestBody.device)) {
-            query.push({ "device": requestBody.device });
+        if (!_.isEmpty(requestBody.sort)) {
+            query.push({"sort": requestBody.sort});
         }
-        if (!_.isEmpty(requestBody.deviceType)) {
-            query.push({ "deviceType": requestBody.deviceType });
+        if (!_.isEmpty(requestBody.code)) {
+            query.push({"code": requestBody.code});
         }
-        if (!_.isEmpty(requestBody.unitCode)) {
-            query.push({ "unitCode": requestBody.unitCode });
-        }
-
-        let result =  await Domain.models.recipe.aggregate([{"$match": {"$and": query}},
-            {"$group":{_id: '$code',  num_coustomer: { $sum: 1 }}},
-        ]);
-
-        logger.debug(`result: ${result}`);
-        return { rs: result, total: result.length }
+        query = query.length==2?{"$and": query} : query.length==1 ? query[0] : {};
+        let result = await Domain.models.vaccination.paginate(query, {
+            sort: {"_id": -1},
+            page: requestBody.page,
+            limit: parseInt(requestBody.size)
+        });
+        return await {rs: result.docs, total: result.total};
     },
 
+    /**
+     * 保存接种记录
+     * @param requestBody
+     * @returns {Promise.<requestBody>}
+     */
+    saveVaccination: async function(requestBody){
+        logger.debug(`saveVaccination param: ${JSON.stringify(requestBody)}`);
+
+        return await Domain.models.vaccination.create(requestBody);
+    },
+
+    
+
+    /**
+     * 修改接种记录
+     * @param requestBody
+     * @returns
+     */
+    modifyVaccination: async function(requestBody){
+         logger.debug(`modifyVaccination param: ${JSON.stringify(requestBody)}`);
+         console.log('OK');
+         return await Domain.models.vaccination.updateOne({_id: requestBody._id}, {
+             $set: {
+                 user: requestBody.user,
+                 device: requestBody.device,
+                 deviceType: requestBody.deviceType,
+                 unitCode: requestBody.unitCode,
+                 unitName: requestBody.unitName,
+                 sort: requestBody.sort,
+                 customer: requestBody.customer
+             }
+         });
+     },
+	 
+	 /**
+     * 按条件查询接种记录
+     * 按接种序号分组统计客户数量aggregate
+     * @param requestBody
+     * @returns
+     */
+    queryVaccinationByCondition: async function(requestBody){
+        logger.debug(`queryVaccinationByCondition param: ${JSON.stringify(requestBody)}`);
+         let query = [];
+         if (!_.isEmpty(requestBody.user)) {
+             query.push({ "user": requestBody.user });
+         }
+         if (!_.isEmpty(requestBody.device)) {
+             query.push({ "device": requestBody.device });
+         }
+         if (!_.isEmpty(requestBody.deviceType)) {
+             query.push({ "deviceType": requestBody.deviceType });
+         }
+         if (!_.isEmpty(requestBody.unitCode)) {
+             query.push({ "unitCode": requestBody.unitCode });
+         }
+         if (!_.isEmpty(requestBody.unitName)) {
+             query.push({ "unitName": requestBody.unitName });
+         }
+         query = query.length == 2 ? { "$and": query } : query.length == 1 ? query[0] : {};
+         return await Domain.models.vaccination.find(query);
+    }
 
 };
