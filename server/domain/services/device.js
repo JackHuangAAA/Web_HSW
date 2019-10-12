3b89e247de157d2e398e5342ec220cb1b7b8d076
hsw1048896938 <1048896938@qq.com>
2019-10-12 16:07:17 +0800

    1、新增汇总信息路由（保存、查询），汇总信息查询（指定条件、分页） 2、设备信息查询新增库存状态，并显示设备内疫苗库存详情（按照UI疫苗柜库存监控需求给数据）

--- server/domain/services/device.js
+++ server/domain/services/device.js
@@ -3,56 +3,61 @@
  */
 const moment = require('moment');
 const logger = Libs.logger.getLogger('device');
-
+const mongoose = require('mongoose');
 module.exports = {
-  /**
-   * 查询设备信息
-   * @param requestBody
-   * @returns {Promise.<{rs: *, total: (*|number)}>}
-   */
-  queryDevices: async function(requestBody) {
-    logger.debug(`queryDevices param: ${JSON.stringify(requestBody)}`);
-    let query = [];
-    if (!_.isEmpty(requestBody.alias)) {
-      query.push({ alias: requestBody.alias });
-    }
-    if (!_.isEmpty(requestBody.code)) {
-      query.push({ code: requestBody.code });
-    }
-    if (!_.isEmpty(requestBody.type)) {
-      query.push({ type: requestBody.type });
-    }
-    if (!_.isEmpty(requestBody.unitCode)) {
-      query.push({ unitCode: requestBody.unitCode });
-    }
-    if (!_.isEmpty(requestBody.cabinetNo)) {
-      query.push({ cabinetNo: requestBody.cabinetNo });
-    }
-    query = query.length > 1 ? { $and: query } : query.length == 1 ? query[0] : {};
-    let result = await Domain.models.device.paginate(query, {
-      sort: { _id: -1 },
-      page: requestBody.page,
-      limit: parseInt(requestBody.size)
-    });
-    return { rs: result.docs, total: result.total };
-  },
 
-  /**
-   * 保存设备信息
-   * @param requestBody
-   * @returns {Promise.<requestBody>}
-   */
-  saveDevice: async function(requestBody) {
-    logger.debug(`saveDevice param: ${JSON.stringify(requestBody)}`);
-    return Domain.models.device.create(requestBody);
-  },
+    /**
+     * 查询设备信息
+     * @param requestBody
+     * @returns {Promise.<{rs: *, total: (*|number)}>}
+     */
+    queryDevices: async function(requestBody){
+        logger.debug(`queryDevices param: ${JSON.stringify(requestBody)}`);
+        let query = [];
+        if (!_.isEmpty(requestBody.alias)) {
+            query.push({"alias": requestBody.alias});
+        }
+        if (!_.isEmpty(requestBody.code)) {
+            query.push({"code": requestBody.code});
+        }
+        if (!_.isEmpty(requestBody.type)) {
+            query.push({"type": requestBody.type});
+        }
+        if (!_.isEmpty(requestBody.unitCode)) {
+            query.push({"unitCode": requestBody.unitCode});
+        }
+        if (!_.isEmpty(requestBody.status)) {
+            query.push({"status": requestBody.status});
+        }
+        if (!_.isEmpty(requestBody.cabinetNo)) {
+            query.push({"cabinetNo": requestBody.cabinetNo});
+        }
+        query = query.length>1?{"$and": query} : query.length==1 ? query[0] : {};
+        let result = await Domain.models.device.paginate(query, {
+            sort: {"_id": -1},
+            page: requestBody.page,
+            limit: parseInt(requestBody.size)
+        });
 
-  /**
-   * 按条件查询设备信息
-   * @param requestBody
-   * @returns {Promise.<T|Query|*>}
-   */
-  queryDeviceByCondition: async function(requestBody) {
+        return {rs: result.docs, total: result.total};
+    },
+
+    /**
+     * 保存设备信息
+     * @param requestBody
+     * @returns {Promise.<requestBody>}
+     */
+    saveDevice: async function(requestBody){
+        logger.debug(`saveDevice param: ${JSON.stringify(requestBody)}`);
+        return await Domain.models.device.create(requestBody);
+    },
+
+    /**
+     * 按条件查询设备信息（状态/类型/单位）
+     * @param requestBody
+     * @returns {Promise.<T|Query|*>}
+     */
+    queryDeviceByCondition: async function(requestBody){
         logger.debug(`queryDeviceByCondition param: ${JSON.stringify(requestBody)}`);
         let query = [];
         if (!_.isEmpty(requestBody.alias)) {
@@ -126,16 +131,61 @@ module.exports = {
         if (!_.isEmpty(requestBody.unitCode)) {
             query.push({"unitCode": requestBody.unitCode});
         }
-
         query = query.length>1?{"$and": query} : query.length==1 ? query[0] : {};
         //查询疫苗不足的设备信息
-        let result = await Domain.models.vaccine.find({total:0},{device:1,_id:0});
+        let result = await Domain.models.vaccine.find({surplus:0});
         let deviceId_array = [];
-        for (let index in result){
-            deviceId_array.push(result[index].device);
+        for (let index in result){//生成疫苗不足的设备ID数组
+            deviceId_array.push(result[index].device.toString());
         };
 
-        return await Domain.models.device.aggregate([{$match:$match},{$group:$group}]);
-    }
-}
+        let result_device = await Domain.models.device.paginate(query, {
+            sort: {"_id": -1},
+            page: requestBody.page,
+            limit: parseInt(requestBody.size)
+        });
 
+        let result_objarray=[];
+        for(let i=0; i<result_device.docs.length;i++)
+        {
+            let newObj = {
+                _id:result_device.docs[i]._id,
+                code:result_device.docs[i].code,
+                alias:result_device.docs[i].alias,
+                type:result_device.docs[i].type,
+                status:result_device.docs[i].address,
+                cabinetNo:result_device.docs[i].cabinetNo,
+                unitCode:result_device.docs[i].unitCode,
+                unitName:result_device.docs[i].unitName,
+                notes:result_device.docs[i].notes,
+                createDate:result_device.docs[i].createDate,
+                updateDate:result_device.docs[i].updateDate,
+                flag:1 //1库存正常，0库存不足
+            };
+
+            if(deviceId_array.includes(result_device.docs[i]._id.toString())){
+                console.log(result_device.docs[i]._id);
+                newObj.flag=0;
+            }
+            result_objarray.push(newObj)
+        };
+        return {rs:result_objarray,total:result_device.total};
+
+    },
+
+    /**
+     * 指定疫苗柜内疫苗库存查询
+     * @param requestBody
+     * @returns
+     */
+    queryDeviceByVaccineStock: async function(requestBody){
+        logger.debug(`queryDeviceByVaccineStock param: ${JSON.stringify(requestBody)}`);
+        let query={device:requestBody.deviceId};
+        let result = await Domain.models.vaccine.paginate(query, {
+            sort: {"_id": -1},
+            page: requestBody.page,
+            limit: parseInt(requestBody.size)
+        });
+        return {rs: result.docs, total: result.total};
+    },
+};
--- server/domain/services/summary.js
+++ server/domain/services/summary.js
@@ -14,5 +14,44 @@ module.exports = {
         logger.debug(`saveSummary param: ${JSON.stringify(requestBody)}`);
         return Domain.models.summary.insertMany(requestBody);
     },
+    /**
+     * 查询汇总信息
+     * @param requestBody
+     * @returns {Promise.<requestBody>}
+     */
+    querySummary: async function(requestBody){
+        logger.debug(`querySummary param: ${JSON.stringify(requestBody)}`);
+        let query = [];
+        if (!_.isEmpty(requestBody.device)) {
+            query.push({"device": requestBody.device});
+        }
+        if (!_.isEmpty(requestBody.deviceType)) {
+            query.push({"deviceType": requestBody.deviceType});
+        }
+        if (!_.isEmpty(requestBody.unitCode)) {
+            query.push({"unitCode": requestBody.unitCode});
+        }
+        if (!_.isEmpty(requestBody.code)) {
+            query.push({"code": requestBody.code});
+        }
+        if (!_.isEmpty(requestBody.begin)) {
+            let begin = moment(requestBody.begin);
+            begin = begin.startOf('day').toDate();
+            query.push({"createDate": {"$gte": begin}});
+        }
+        if (!_.isEmpty(requestBody.end)) {
+            let end = moment(requestBody.end);
+            end = end.endOf('day').toDate();
+            query.push({"createDate": {"$lte": end}});
+        }
+        query = query.length>1?{"$and": query} : query.length==1 ? query[0] : {};
+        let result = await Domain.models.summary.paginate(query, {
+            sort: {"_id": -1},
+            page: requestBody.page,
+            limit: parseInt(requestBody.size),
+            lean:true
+        });
+        return {rs: result.docs, total: result.total};
+    },
 
 };
