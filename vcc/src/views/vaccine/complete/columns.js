73680f4ed8f99066b3c1dfbd2a1cd06e461cbfcf
李振宇 <407852978@qq.com>
2019-10-12 11:49:30 +0800

    暂存

---
+++ vcc/src/views/vaccine/complete/columns.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    type:'index',
+    title:'序号',
+    width:'120px'
+  },
+  {
+    title:'疫苗名称',
+    key:''
+  }
+]
--- vcc/src/views/vaccine/complete/complete.vue
+++ vcc/src/views/vaccine/complete/complete.vue
@@ -10,26 +10,33 @@
     </div>
     <div class="complete-info">
       <Row>
-        <Col span="4">
+        <Col span="8">
         {{title}}支数:
         </Col>
-        <Col span="4">
+        <Col span="8">
         取出人员:{{username}}
         </Col>
-        <Col span="4">
+        <Col span="8">
         取出时间:{{time}}
         </Col>
       </Row>
+    </div>
+    <div class="complete-main">
+      <show-table></show-table>
     </div>
   </div>
 </template>
 
 <script>
+import showTable from '_c/main/showTable'
 import gohome from "_a/gohome.png";
 import succeed from "_a/succeed.png";
 import moment from "moment";
 export default {
   name: "complete",
+  components:{
+    showTable
+  },
   data() {
     return {
       succeed,
@@ -39,9 +46,8 @@ export default {
   },
   computed: {
     time() {
-      let a = moment().format("YYYY-MM-DD HH:mm:ss");
-      console.log(a);
-      return a;
+      let time = moment().format("YYYY-MM-DD HH:mm:ss");
+      return time;
     },
     username() {
       if (this.userinfo != null) return this.userinfo.name;
@@ -83,5 +89,13 @@ export default {
   position: absolute;
   top: 30px;
   right: 20px;
+}
+.complete-info {
+  padding:40px 0px;
+  position: relative;
+  left: 24%;
+  width: 70%;
+  font-size: 17px;
+  font-weight: 400;
 }
 </style>
