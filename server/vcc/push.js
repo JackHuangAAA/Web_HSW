10f335ddd8da8e551850a6cab01a0aacd4a8c933
hsw1048896938 <1048896938@qq.com>
2019-10-25 09:45:33 +0800

    修改消息推送，删除推送具体信息，改为推送刷新指令

--- server/vcc/push.js
+++ server/vcc/push.js
@@ -41,46 +41,13 @@ http.listen(9996 || 8080, '0.0.0.0');
 
 async function execute() {
     _.mapKeys(map, async function (value, key) {
-        //查询温度报警、当天服务人数、查询抽屉疫苗信息
-        let deviceData = await Domain.services.device.queryDeviceByCondition({ code: key });
-        //查询温度报警
-        let alarmNumber = (await Domain.services.alarm.queryAlarmByByCondition({ device: deviceData[0]._id,type:1,ifToday:'today' })).length;
-        //当天服务人数
-        let customerNumber = (await Domain.services.vaccination.queryVaccinationDailyInfo({ device: deviceData[0]._id,ifToday:'today' })).length;
-        // 查询抽屉疫苗信息
-        let res = await Domain.services.drawer.queryDrawerByCondition({ device: deviceData[0]._id });
-        let vaccineData=[];
-        let array = res;
-        for (let i = 0; i < 10; i++) {
-            let num = array[i].vaccine.length, vaccine = array[i].vaccine, temp = {};
-            if (num > 0) {
-                for (let k = 0; k < num; k++) {
-                    if (k == 0) {
-                        temp.vaccineOneName = vaccine[k].name;
-                        temp.vaccineOneCount = vaccine[k].surplus;
-                    }
-                    if (k == 1) {
-                        temp.vaccineTwoName = vaccine[k].name;
-                        temp.vaccineTwoCount = vaccine[k].surplus;
-                    }
-                }
-            } else {
-                temp.vaccineOneName = '';
-                temp.vaccineOneCount = '';
-                temp.vaccineTwoName = '';
-                temp.vaccineTwoCount = '';
-            }
-            vaccineData.push(temp);
-        }
-
+        //定时发送刷新指令
 
          let timedData = {
-             alarmNumber: alarmNumber,
-             customerNumber: customerNumber,
-             vaccineData: vaccineData,
+             type: "refresh"
          };
         value.emit( Domain.enum.TIMEDATA, timedData);
-        value.emit( 'test2', '再一次');
+        //value.emit( 'test2', '再一次');
     });
 }
 
