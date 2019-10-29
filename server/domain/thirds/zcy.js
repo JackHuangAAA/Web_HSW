3397e8749c87dfb2f7a9d49863e4cbff20adb328
hsw1048896938 <1048896938@qq.com>
2019-10-29 11:58:57 +0800

    修改疫苗信息案例

--- server/domain/thirds/zcy.js
+++ server/domain/thirds/zcy.js
@@ -7,221 +7,221 @@ const logger = Libs.logger.getLogger('vaccine')
 var mongoose = require('mongoose')
 
 module.exports = {
-      /**
-       * 用户登陆校验
-       * @param requestBody
-       * @returns {Promise.<boolean>}
-       */
-      checkUser: async function(requestBody) {
+    /**
+     * 用户登陆校验
+     * @param requestBody
+     * @returns {Promise.<boolean>}
+     */
+    checkUser: async function(requestBody) {
         logger.debug(`checkUser param: ${JSON.stringify(requestBody)}`);
         if (requestBody.code == 'admin' && requestBody.password == '000000') {
-          return { code: 'admin', name: '小米', check: true };
+            return { code: 'admin', name: '小米', check: true };
         }
         return { check: false };
-      },
+    },
 
-      /**
-       * 查询行政区域信息
-       * @param requestBody
-       * @returns {Promise.<{}>}
-       */
-      queryDistrict: async function(requestBody) {
+    /**
+     * 查询行政区域信息
+     * @param requestBody
+     * @returns {Promise.<{}>}
+     */
+    queryDistrict: async function(requestBody) {
         logger.debug(`queryDistrict param: ${JSON.stringify(requestBody)}`);
         let result = {
-          name: '浙江省',
-          code: '330000000000',
-          child: [
-            {
-              name: '杭州市',
-              code: '330101000000',
-              child: [
+            name: '浙江省',
+            code: '330000000000',
+            child: [
                 {
-                  name: '上城区',
-                  code: '330101000000',
-                  child: [
-                    {
-                      name: '清波街道',
-                      code: '330102001000'
-                    },
-                    {
-                      name: '湖滨街道',
-                      code: '330102003000'
-                    }
-                  ]
+                    name: '杭州市',
+                    code: '330101000000',
+                    child: [
+                        {
+                            name: '上城区',
+                            code: '330101000000',
+                            child: [
+                                {
+                                    name: '清波街道',
+                                    code: '330102001000'
+                                },
+                                {
+                                    name: '湖滨街道',
+                                    code: '330102003000'
+                                }
+                            ]
+                        },
+                        {
+                            name: '滨江区',
+                            code: '330102000000',
+                            child: [
+                                {
+                                    name: '西兴街道',
+                                    code: '330102001000'
+                                },
+                                {
+                                    name: '长河街道',
+                                    code: '330102003000'
+                                }
+                            ]
+                        }
+                    ]
                 },
                 {
-                  name: '滨江区',
-                  code: '330102000000',
-                  child: [
-                    {
-                      name: '西兴街道',
-                      code: '330102001000'
-                    },
-                    {
-                      name: '长河街道',
-                      code: '330102003000'
-                    }
-                  ]
+                    name: '绍兴市',
+                    code: '330600000000',
+                    child: [
+                        {
+                            name: '越城区',
+                            code: '330602000000',
+                            child: [
+                                {
+                                    name: '塔山街道',
+                                    code: '330602001000'
+                                },
+                                {
+                                    name: '蕺山街道',
+                                    code: '330602003000'
+                                }
+                            ]
+                        },
+                        {
+                            name: '柯桥区',
+                            code: '330102000000',
+                            child: [
+                                {
+                                    name: '柯岩街道',
+                                    code: '330603002000'
+                                },
+                                {
+                                    name: '湖塘街道',
+                                    code: '330603004000'
+                                }
+                            ]
+                        }
+                    ]
                 }
-              ]
-            },
-            {
-              name: '绍兴市',
-              code: '330600000000',
-              child: [
-                {
-                  name: '越城区',
-                  code: '330602000000',
-                  child: [
-                    {
-                      name: '塔山街道',
-                      code: '330602001000'
-                    },
-                    {
-                      name: '蕺山街道',
-                      code: '330602003000'
-                    }
-                  ]
-                },
-                {
-                  name: '柯桥区',
-                  code: '330102000000',
-                  child: [
-                    {
-                      name: '柯岩街道',
-                      code: '330603002000'
-                    },
-                    {
-                      name: '湖塘街道',
-                      code: '330603004000'
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
+            ]
         };
         return result;
-      },
+    },
 
-      /**
-       * 查询单位信息
-       * @param requestBody
-       * @returns {Promise.<{}>}
-       */
-      queryUnit: async function(requestBody) {
+    /**
+     * 查询单位信息
+     * @param requestBody
+     * @returns {Promise.<{}>}
+     */
+    queryUnit: async function(requestBody) {
         logger.debug(`queryUnit param: ${JSON.stringify(requestBody)}`);
         let result = [
-          { code: '3200001', name: '武林' },
-          { code: '3200002', name: '望江' },
-          { code: '3200003', name: '南星' },
-          { code: '3200004', name: '湖滨' },
-          { code: '3200005', name: '朝晖' }
+            { code: '3200001', name: '武林' },
+            { code: '3200002', name: '望江' },
+            { code: '3200003', name: '南星' },
+            { code: '3200004', name: '湖滨' },
+            { code: '3200005', name: '朝晖' }
         ];
         return result;
-      },
+    },
 
-      /**
-       * 查询柜台编号信息
-       * @param requestBody
-       * @returns {Promise.<[*,*,*,*,*]>}
-       */
-      queryCabinetNo: async function(requestBody) {
+    /**
+     * 查询柜台编号信息
+     * @param requestBody
+     * @returns {Promise.<[*,*,*,*,*]>}
+     */
+    queryCabinetNo: async function(requestBody) {
         logger.debug(`queryCabinetNo param: ${JSON.stringify(requestBody)}`);
         let result = [
-          { value: '1100001' },
-          { value: '1100002' },
-          { value: '1100003' },
-          { value: '1100004' },
-          { value: '1100005' }
+            { value: '1100001' },
+            { value: '1100002' },
+            { value: '1100003' },
+            { value: '1100004' },
+            { value: '1100005' }
         ]
         return result;
-      },
+    },
 
-      /**
-       * 查询疫苗信息
-       * @param requestBody
-       * @returns {Promise.<[*,*,*,*,*]>}
-       */
-      queryVaccine: async function(requestBody) {
+    /**
+     * 查询疫苗信息
+     * @param requestBody
+     * @returns {Promise.<[*,*,*,*,*]>}
+     */
+    queryVaccine: async function(requestBody) {
         logger.debug(`queryVaccine param: ${JSON.stringify(requestBody)}`);
         let result = {
-          code: 'ym20190920134508999',
-          name: '脊髓灰质炎疫苗',
-          supervisionCode: 'jg20190920001',
-          expiry: new Date(),
-          producer: '长春生物制药'
+            code: 'ym991',
+            name: '卡介苗',
+            supervisionCode: 'jg20190920001',
+            expiry: new Date(),
+            producer: '上海生物'
         };
         return result;
-      },
-      /**
-       * 查询疫苗总种类信息
-       * @param requestBody
-       * @returns {Promise.<[*,*,*,*,*]>}
-       */
-      queryVaccineKinds: async function(requestBody) {
+    },
+    /**
+     * 查询疫苗总种类信息
+     * @param requestBody
+     * @returns {Promise.<[*,*,*,*,*]>}
+     */
+    queryVaccineKinds: async function(requestBody) {
         logger.debug(`queryVaccine param: ${JSON.stringify(requestBody)}`);
         let result = [
-          {
-          code: 'ym20190920134508991',
-          name: '脊髓灰质炎疫苗',
-          supervisionCode: 'jg20190920001',
-          expiry: new Date(),
-          producer: '长春生物制药'
-        },
-          {
-          code: 'ym20190920134508992',
-          name: '麻疹疫苗',
-          supervisionCode: 'jg20190920001',
-          expiry: new Date(),
-          producer: '长春生物制药'
-        },
-          {
-          code: 'ym20190920134508993',
-          name: '乙肝疫苗',
-          supervisionCode: 'jg20190920001',
-          expiry: new Date(),
-          producer: '长春生物制药'
-        },
-          {
-          code: 'ym20190920134508994',
-          name: '水痘',
-          supervisionCode: 'jg20190920001',
-          expiry: new Date(),
-          producer: '长春生物制药'
-        },
-        {
-          code: 'ym20190920134508995',
-          name: '流感疫苗',
-          supervisionCode: 'jg20190920001',
-          expiry: new Date(),
-          producer: '长春生物制药'
-        },
-        {
-          code: 'ym20190920134508996',
-          name: '腮腺炎疫苗',
-          supervisionCode: 'jg20190920001',
-          expiry: new Date(),
-          producer: '长春生物制药'
-        }
-      ];
+            {
+                code: 'ym991',
+                name: '卡介苗',
+                supervisionCode: 'jg20190920001',
+                expiry: new Date(),
+                producer: '上海生物'
+            },
+            {
+                code: 'ym992',
+                name: '免费乙肝',
+                supervisionCode: 'jg20190920001',
+                expiry: new Date(),
+                producer: '大连汉信'
+            },
+            {
+                code: 'ym993',
+                name: '脊灰灭活疫苗',
+                supervisionCode: 'jg20190920001',
+                expiry: new Date(),
+                producer: '医科院'
+            },
+            {
+                code: 'ym994',
+                name: '百白破',
+                supervisionCode: 'jg20190920001',
+                expiry: new Date(),
+                producer: '武汉生物'
+            },
+            {
+                code: 'ym995',
+                name: '麻风',
+                supervisionCode: 'jg20190920001',
+                expiry: new Date(),
+                producer: '北京天坛'
+            },
+            {
+                code: 'ym996',
+                name: '麻腮风',
+                supervisionCode: 'jg20190920001',
+                expiry: new Date(),
+                producer: '上海生物'
+            }
+        ];
         return result;
-      },
+    },
 
-      /**
-       * 接收接种信息
-       * @param requestBody
-       * @returns {Promise.<[*,*,*,*,*]>}
-       */
-      receiveVaccination: async function(requestBody) {
+    /**
+     * 接收接种信息
+     * @param requestBody
+     * @returns {Promise.<[*,*,*,*,*]>}
+     */
+    receiveVaccination: async function(requestBody) {
         logger.debug(`queryVaccine param: ${JSON.stringify(requestBody)}`);
         let result = {
             customer:{
                 code: '089',
                 name: '李义',
                 age: 4,
-                vaccineCode: 'ym20190920134508999',
-                vaccineName: '脊髓灰质炎疫苗',
+                vaccineCode: 'ym991',
+                vaccineName: '卡介苗',
                 vaccineNum: 1
             },
             cabinetNo:"1100002"
@@ -236,7 +236,7 @@ module.exports = {
 
         await Domain.redis.pub.publishAsync(channel, message);
 
-      },
+    },
 
     /**
      * 收任务完成状态
