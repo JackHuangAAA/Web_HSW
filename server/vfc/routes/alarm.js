53f1b0eae0698e1cc5b476a528c2d51c7036065e
hsw1048896938 <1048896938@qq.com>
2019-10-30 17:27:53 +0800

    删除报警信息多余的接口

--- server/vfc/routes/alarm.js
+++ server/vfc/routes/alarm.js
@@ -3,7 +3,7 @@ const router = require('koa-router')();
 const logger = Libs.logger.getLogger('alarm');
 
 /**
- * @api {GET} /alarm/queryAlarmByByCondition  查询当天报警次数及报警信息
+ * @api {GET} /alarm/queryAlarmByCondition  查询当天报警次数及报警信息
  * @apiGroup Alarm
  * @apiVersion 1.0.0
  * @apiDescription 查询当天报警次数及报警信息
@@ -15,9 +15,9 @@ const logger = Libs.logger.getLogger('alarm');
  * @apiSuccess {Array}  rs  当天报警次数信息数组
  * @apiSuccess {Number}  total 当天报警次数
  */
-router.get('/queryAlarmByByCondition',
+router.get('/queryAlarmByCondition',
     Libs.router(async (ctx, next) => {
-       return await Domain.services.alarm.queryAlarmByByCondition(ctx.request.query);
+       return await Domain.services.alarm.queryAlarmByCondition(ctx.request.query);
     })
 );
 
@@ -36,21 +36,21 @@ router.post('/saveAlarm',
 );
 
 /**
- * @api {GET} /alarm/queryAlarmDailyInfo  查询今日报警信息
+ * @api {GET} /alarm/queryAlarm  查询报警信息
  * @apiGroup alarm
  * @apiVersion 1.0.0
  * @apiDescription 查询报警信息
- * @apiParam {String} [ifToday] 是否查询今日信息，若无则查询所有时间
  * @apiParam {String} [device] 设备
  * @apiParam {Number} [deviceType] 设备类型
  * @apiParam {String} [unitCode] 所属单位编号
- ** @apiParam {String} [unitName] 所属单位名称
  * @apiParam {Number} [type] 报警类型(1:温度异常;2:库存不足)
+ * @apiParam {String} [device] 设备
+ * @apiParam {String} [device] 设备
  * @apiSuccess {JSON}  Object  inout model 数组
  */
-router.get('/queryAlarmDailyInfo',
+router.get('/queryAlarm',
     Libs.router( async (ctx, next) => {
-        return await Domain.services.alarm.queryAlarmDailyInfo(ctx.request.query);
+        return await Domain.services.alarm.queryAlarm(ctx.request.query);
     })
 );
 module.exports = router;
