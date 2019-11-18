beab772f441bb36e73676b7f0872cc982748aa84
xiangbingzhi <xiangbingzhi@ethinkbank.com>
2019-11-18 16:28:02 +0800

    接种

--- vcc/src/views/vaccination/vaccination.vue
+++ vcc/src/views/vaccination/vaccination.vue
@@ -24,66 +24,56 @@
                             {{vaccinationData.customer?vaccinationData.customer.age:''}}周岁
                         </div>
                     </div>
-                </div>
-                <div class="leftBottom">
-                    <div class="personInf" v-for="(item,index) in vaccinationData.vaccine">
-                        <div class="personInfVaccine">
-                            <p class="personInfVaccineP vaccineOne">接种疫苗</p>
-                            <p class="personInfVaccineP vaccineTwo">{{item.name}}</p>
-                            <p class="personInfVaccineP vaccineThree">{{item.product}}</p>
+                    <div class="leftTopRightTwo">
+                        <div class="age">
+                            性别:
                         </div>
-                        <div class="personInfCount">
-                            <p class="getVaccineOne">数量</p>
-                            <p class="getVaccineCount">{{item.num}}支</p>
-                        </div>
-                        <div class="personInfStatus">
-                            <p class="getVaccineOne">状态</p>
-                            <p class="getVaccineCount">
-                                <img src="/static/img/succeed.png" class="succeed" v-if="item.status"><span v-if="item.status" class="tipInf" style="color:#1AA95E">信息匹配</span>
-                                <img src="/static/img/error.png" class="succeed" v-if="!item.status"><span v-if="!item.status" class="tipInf" style="color:#F42954">信息不匹配</span>
-                            </p>
+                        <div class="ageCount">
+                            {{vaccinationData.customer?vaccinationData.customer.sex:''}}
                         </div>
                     </div>
+                </div>
+                <div class="leftBottom">
+                    <CheckboxGroup v-model="model1">
+                        <div class="personInf" v-for="(item,index) in vaccinationData.vaccine">
+                            <div>
+                                <span v-if="!item.status" class="tipInf" style="color:#F42954">信息不匹配</span><img src="/static/img/error.png" class="succeed" v-if="!item.status">
+                                <span v-if="item.status" class="tipInf" style="color:#1AA95E">信息匹配</span><img src="/static/img/succeed.png" class="succeed" v-if="item.status">
+                                <Checkbox class="personInf-check" :label="item.name"></Checkbox>
+                            </div>
+                            <div class="producter">
+                                生产厂家：{{item.product}}
+                            </div>
+                            <div class="vaccine-info">
+                                <div class="vaccine-info-part">
+                                    <div>接种部位：</div>
+                                    <Select v-model="model1" style="width:9rem;height:2.25rem">
+                                        <Option v-for="item in part" :value="item.value" :key="item.key">{{ item.value }}</Option>
+                                    </Select>
+                                </div>
+                                <div class="vaccine-info-num">
+                                    接种剂量：{{item.num}}支
+                                </div>
+                            </div>
+                            <div class="vaccine-btnBox">
+                                <button>取消接种</button>
+                                <button>确认接种</button>
+                            </div>
+                        </div>
+                    </CheckboxGroup>
                 </div>
             </div>
             <div class="right">
                 <div class="vaccineInf" v-for="(item,index) in vaccine">
-                    <div class="vaccineTop">
-                        <div class="vaccineTopLeft">
-                            <div class="topTip">
-                                品名:
-                            </div>
-                            <div class="bottomInf">
-                                {{item.name}}
-                            </div>
-                        </div>
-                        <div class="vaccineTopRight">
-                            <div class="topTip">
-                                批次号:
-                            </div>
-                            <div class="bottomInf" style="color:#3E4955">
-                                {{item.batchNo}}
-                            </div>
-                        </div>
+                    <div class="vaccineInf-top">
+                        <div><span>{{item.name}}</span></div>
+                        <div>监管码：<span>Y750230-64368</span></div>
+                        <div class="mt24">批次号：<span>Y750230-64368</span></div>
+                        <div class="mt24">疫苗剂量：<span>0.5ml/支</span></div>
                     </div>
-                    <div class="vaccineBottom">
-                        <div class="vaccineTopLeft">
-                            <div class="topTip">
-                                有效期:
-                            </div>
-                            <div class="bottomInf" style="color:#3E4955;">
-                                {{getExpiryDate(item.expiry)}}
-                            </div>
-                        </div>
-                        <div class="vaccineTopRight">
-                            <div class="topTip">
-                                生产企业:
-                            </div>
-                            <div class="bottomInf" style="color:#3E4955">
-                                {{item.producer}}
-                            </div>
-                        </div>
-                    </div>
+                    <div class="vaccineInf-bottom">有效期：<span>2019-09-18 12:30:28   至  2022-12-29 12:20:19</span></div>
+                    <div class="vaccineInf-bottom">生产厂家：<span>{{item.producer}}</span></div>
+                    <div class="vaccineInf-bottom">接种份数：<span>1</span></div>
                 </div>
             </div>
         </div>
@@ -107,7 +97,8 @@
                     'customer':{
                         'code': '089',
                         'name': '李义',
-                        'age': 4
+                        'age': 4,
+                        'sex':'女'
                     },
                     'vaccine':[
                         {
@@ -148,7 +139,19 @@
                         'batchNo':'b-003',
                         'producer':'长春生物制药'
                     }
-                ]//疫苗扫码信息
+                ],//疫苗扫码信息
+                part: [
+                    {
+                        value: '左上臂',
+                        key: '左上臂'
+                    },
+                    {
+                        value: '右上臂',
+                        key: '右上臂'
+                    }
+                ],
+                model1: [],
+                single:false
             };
         },
         computed: {
@@ -238,6 +241,9 @@
                     }                    
                 });
             },
+            queryQueue(){
+                // this.$api.get()
+            }
         },
         mounted() {
             this.commonData = {
@@ -252,9 +258,10 @@
             // this.vaccinationData = this.$route.query.vaccination;
             //打开需要接种的疫苗所在抽屉
             //this.openDrawer(this.vaccinationData);
-            this.receiveVaccinationStatus();
+            //this.receiveVaccinationStatus();
             //接收疫苗扫描后结果，与接种信息比对
             this.matchInfo();
+            //获取排队数据
         }
     };
 </script>
