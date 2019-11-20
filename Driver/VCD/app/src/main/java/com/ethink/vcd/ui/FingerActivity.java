1245d53dd77b530a7c41ba2ccfcbc19d6d5c440d
caijie <caijie@ethinkbank.com>
2019-11-20 09:11:46 +0800

    11-20

--- Driver/VCD/app/src/main/java/com/ethink/vcd/ui/FingerActivity.java
+++ Driver/VCD/app/src/main/java/com/ethink/vcd/ui/FingerActivity.java
@@ -18,7 +18,6 @@ import com.ethink.vcd.App;
 import com.ethink.vcd.Const;
 import com.ethink.vcd.R;
 import com.ethink.vcd.SPUtils;
-import com.ethink.vcd.controller.FingerCommon;
 import com.ethink.vcd.controller.FingerUtil;
 import com.ethink.vcd.event.FingerPushMessage;
 import com.ethink.vcd.service.HttpUtils;
@@ -27,8 +26,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
