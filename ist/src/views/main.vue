3cc5c727d11a530658c65709f69bf95e2d1af221
afu <chaizhifeng@ethinkbank.com>
2019-11-25 14:25:34 +0800

    接种信息打印页面

--- ist/src/views/main.vue
+++ ist/src/views/main.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <img src="/static/img/logo.png" class="logo" alt="">
         <div class="device">设备编号：{{device.id}}</div>
-        <div class="title">智能接种，排队挂号，扫描疫苗本</div>
+        <div class="title">扫描疫苗本，排队挂号，智能接种</div>
         <div class="scanNotice">
             <div>扫码挂号</div>
             <div>请将疫苗本条码对准扫描口，进行扫码</div>
--- ist/src/views/print/printEnd.vue
+++ ist/src/views/print/printEnd.vue
@@ -3,7 +3,7 @@
         <div class="complete">
             <div class="countDown">
                 <img src="/static/img/clock.png" alt="">
-                <div>倒计时00:{{count}}</div>
+                <div>倒计时00:{{zero?'0':''}}{{count}}</div>
             </div>
             <div class="complete-success">
                 <img src="/static/img/success.png" alt="">
@@ -12,38 +12,59 @@
             <div class="complete-detail">
                 <detail/>
             </div>
-            <div class="complete-confirm" style="margin-top:11.5625rem;" @click="confirm()">确认</div>
+            <div class="complete-confirm" style="margin-top:11.5625rem;" @click="confirm()">返回首页</div>
         </div>
     </div>
 </template>
 <script>
+import { mapGetters, mapActions, mapState } from 'vuex';
 import detail from '../../components/detail';
+
 export default {
     data () {
         return {
-            count: 15,
+            zero: false,
+            count: 10,
             timer: ''
         }    
     },
     components:{
         detail
     },
-    mounted(){
-        this.timer = setInterval(() => {
-            this.count --;
-            if(this.count == 0){
-                clearInterval(this.timer);
-                this.$router.push('/print/printMain')
-                }
-        }, 1000);
+    computed: {
+        ...mapGetters({
+            user: 'user',
+            device: 'device'
+        })
     },
     methods:{
+        ...mapActions({
+            saveUser: 'saveUser',
+            saveDevice: 'saveDevice'
+        }),
+        countdown(){
+            this.timer = setInterval(() => {
+                this.count --;
+                if(this.count<10){
+                    this.zero = true;
+                }
+                if(this.count == 0){
+                    clearInterval(this.timer);
+                    this.$router.push('/print/printMain');
+                }
+            }, 1000);
+        },
         confirm: function(){
+            clearInterval(this.timer);
+            this.saveUser(null);
             this.$router.push('/print/printMain')
         }
-    }
+    },
+    mounted(){
+        this.countdown();
+    },
 }
 </script>
 <style lang="less" scoped>
-@import '~@/style/complete.less';
+@import '~@/style/printEnd.less';
 </style>
