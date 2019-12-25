d75e8863763debfa441741b280b4656053cf977e
xiangbingzhi <xiangbingzhi@ethinkbank.com>
2019-12-25 17:42:40 +0800

    display添加语音播报

--- display/src/main.js
+++ display/src/main.js
@@ -12,12 +12,13 @@ import iView from 'iview';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 import config from '@/config';
 import rem from '@/rem';
+import device from '@/device.js';
 import FastClick from 'fastclick'
 
 FastClick.attach(document.body);
 Vue.use(iView);
 Vue.use(VueAwesomeSwiper);
-//if (config.env == 'development') {
+if (config.env == 'development') {
     //生产环境显示屏应用仅使用下面的配置，if else 需要注释
     global.__app = new Vue({
         el: '#app',
@@ -25,19 +26,20 @@ Vue.use(VueAwesomeSwiper);
         store: store,
         render: h => h(App)
     });
-// }else {
-//     // 安卓环境使用
-//     window.$d.onReady = function() {
-//         console.log('$d.ready');
-//         global.__app = new Vue({
-//             el: '#app',
-//             router: router,
-//             store: store,
-//             render: h => h(App)
-//         });
-//     };
-// }
+}else {
+    // 安卓环境使用
+    window.$d.onReady = function() {
+        console.log('$d.ready');
+        global.__app = new Vue({
+            el: '#app',
+            router: router,
+            store: store,
+            render: h => h(App)
+        });
+    };
+}
 Vue.prototype.$cookies = Cookies;
 Vue.prototype.$api = api;
 Vue.prototype.$config = config;
+Vue.prototype.$device = device;
 Vue.prototype.$rem = rem;
--- display/src/views/display/queue.vue
+++ display/src/views/display/queue.vue
@@ -37,6 +37,12 @@ export default {
 
                 });
             });
+            this.socket.on("NextVaccination",data=>{
+                console.log(data);
+                if(this.queue.length>0){
+                    this.$device.audio({audio_text:`${this.queue[0].sort}号${this.queue[0].name}${this.queue[0].position}`});
+                }
+            })
         },
         async queryQueue(){
             let queue = await this.$api.get('/queue/queryQueueByCondition',{status:1});
